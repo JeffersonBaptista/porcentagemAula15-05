418d37d206758e4d51f53c9b9fe599e31ee000b6
jeffersonbaptista <jefferson.baptista@zup.com.br>
2019-05-16 16:07:21 -0300

    calcular a porcentagem a ser recedida

---
+++ src/com/br/porcentagem/PorcentagemVenda.java
@@ -0,0 +1,42 @@
+package com.br.porcentagem;
+import java.util.Scanner;
+
+public class PorcentagemVenda {
+	public static void main(String[] args) {
+		String id = "";
+		String peca = "";
+		double valor = 0;
+		int quant = 0;
+		double comissao = 0;
+		double total = 0;
+		
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("informe o nome do vendedor");
+		
+		id = s.next();
+		
+		System.out.println("informe o produto vendida");
+		
+		peca = s.next();
+		
+		System.out.println("informe valor da peça");
+		
+		valor = s.nextDouble();
+		
+		System.out.println("informe a quantidade vendida");
+
+		quant = s.nextInt();
+		
+		total = valor * quant;
+		
+		comissao = (total * 0.5)/100;
+		
+		System.out.println("total vendido pelo " + id +": " + total);
+		System.out.println("total de comissão a receber:" + comissao);
+		
+		
+	}
+
+}
